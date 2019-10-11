a93e50f3003bbc646c7ff6d22c11e589cf8f3432
deardelia <deardelia424@126.com>
2019-10-10 22:46:08 -0500

    Add files via upload

---
+++ searchTree/DecisionTreeImpl.java
@@ -0,0 +1,77 @@
+import java.util.List;
+
+/**
+ * Fill in the implementation details of the class DecisionTree using this file. Any methods or
+ * secondary classes that you want are fine but we will only interact with those methods in the
+ * DecisionTree framework.
+ */
+public class DecisionTreeImpl {
+	public DecTreeNode root;
+	public List<List<Integer>> trainData;
+	public int maxPerLeaf;
+	public int maxDepth;
+	public int numAttr;
+
+	// Build a decision tree given a training set
+	DecisionTreeImpl(List<List<Integer>> trainDataSet, int mPerLeaf, int mDepth) {
+		this.trainData = trainDataSet;
+		this.maxPerLeaf = mPerLeaf;
+		this.maxDepth = mDepth;
+		if (this.trainData.size() > 0) this.numAttr = trainDataSet.get(0).size() - 1;
+		this.root = buildTree();
+	}
+	
+	private DecTreeNode buildTree() {
+		// TODO: add code here	
+		return null;
+	}
+	
+	public int classify(List<Integer> instance) {
+		// TODO: add code here
+		// Note that the last element of the array is the label.
+		return 0;
+	}
+	
+	// Print the decision tree in the specified format
+	public void printTree() {
+		printTreeNode("", this.root);
+	}
+
+	public void printTreeNode(String prefixStr, DecTreeNode node) {
+		String printStr = prefixStr + "X_" + node.attribute;
+		System.out.print(printStr + " <= " + String.format("%d", node.threshold));
+		if(node.left.isLeaf()) {
+			System.out.println(" : " + String.valueOf(node.left.classLabel));
+		}
+		else {
+			System.out.println();
+			printTreeNode(prefixStr + "|\t", node.left);
+		}
+		System.out.print(printStr + " > " + String.format("%d", node.threshold));
+		if(node.right.isLeaf()) {
+			System.out.println(" : " + String.valueOf(node.right.classLabel));
+		}
+		else {
+			System.out.println();
+			printTreeNode(prefixStr + "|\t", node.right);
+		}
+	}
+	
+	public double printTest(List<List<Integer>> testDataSet) {
+		int numEqual = 0;
+		int numTotal = 0;
+		for (int i = 0; i < testDataSet.size(); i ++)
+		{
+			int prediction = classify(testDataSet.get(i));
+			int groundTruth = testDataSet.get(i).get(testDataSet.get(i).size() - 1);
+			System.out.println(prediction);
+			if (groundTruth == prediction) {
+				numEqual++;
+			}
+			numTotal++;
+		}
+		double accuracy = numEqual*100.0 / (double)numTotal;
+		System.out.println(String.format("%.2f", accuracy) + "%");
+		return accuracy;
+	}
+}
